f2370e940cb5d5c052190de470664a99bc124565
Taisiia <159428493+Taisiia-Mazur@users.noreply.github.com>
2024-10-26 22:15:24 +0300

    add settings

--- src/scripts/countContacts.js
+++
@@ -1,8 +0,0 @@
-import { readContacts } from "../utils/readContacts.js";
-
-export const countContacts = async () => {
-    const contact = await readContacts();
-    return contact.length;
-};
-
-console.log(await countContacts());
